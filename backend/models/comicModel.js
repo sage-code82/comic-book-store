a78b821fb6b9b0ede0eab1b11dabdeb48e02213a
Stephen <sagelvl40@gmail.com>
2024-02-06 15:37:12 +0000

    set up mongodb

---
+++ backend/models/comicModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const comicSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    publisher: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
