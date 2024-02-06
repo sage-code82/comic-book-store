dc974d91c4e7e56be956b9da9cd677ba7cf02b8a
Stephen <sagelvl40@gmail.com>
2024-02-06 15:02:38 +0000

    init bruv

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://sage82:Merlot82@cluster0.ufzio3j.mongodb.net/comic-books?retryWrites=true&w=majority";
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Hello There!");
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`Hello there Port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
