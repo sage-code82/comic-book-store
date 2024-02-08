5b44da5f46630a2fc607ef44f4711990f36ec2e5
Stephen <sagelvl40@gmail.com>
2024-02-08 15:51:21 +0000

    set up react and tailwind

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Comic } from "./models/comicModel.js";
 import comicRoute from "./routes/comicRoute.js";
 import cors from "cors";
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="bg-purple-600 text-white">Hello There!</div>;
+};
+
+export default App;
